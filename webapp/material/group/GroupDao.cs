4b9d35dddde95bfe266c27db8019baefe515e229
Pregrammer-App <Pregrammer-App@PROGRAMER-APP>
2021-07-23 17:20:35 +0700

    new dao

---
+++ webapp/material/group/GroupDao.cs
@@ -0,0 +1,30 @@
+﻿using KKN_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace KKN_UI.material.group
+{
+    public class GroupDao
+    {
+        private const string Read = "";
+        private const string Creat = "";
+        private const string Update = "";
+        private const string Delete = "";
+
+
+
+
+        public GroupSQL maplistgroup(SqlDataReader rdr)
+        {
+            var resultgroup = new GroupSQL();
+            resultgroup.group_id = Convert.ToInt32(rdr["group_id"]);
+            resultgroup.group_name = rdr["group_name"].ToString();
+
+            return resultgroup;
+        }
+
+    }
+}
