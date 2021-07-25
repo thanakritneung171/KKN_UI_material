6b3dc6e39fdacc8e6a426c93f267a744a4713b11
58030171 <58030171@kmitl.ac.th>
2021-07-25 23:28:20 +0700

    Dao 25/07

---
+++ webapp/material/Material_acc/Material_accDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KKN_UI.material.Material_acc
+{
+    public class Material_accDao
+    {
+    }
+}
---
+++ webapp/material/category/CategoryDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KKN_UI.material.category
+{
+    public class CategoryDao
+    {
+    }
+}
