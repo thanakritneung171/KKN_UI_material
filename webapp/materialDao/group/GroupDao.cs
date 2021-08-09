ee697ec20d5a38f67303f9c395f2e4d8c5db08ea
58030171 <58030171@kmitl.ac.th>
2021-08-09 23:39:19 +0700

    use comfirm complete but bug alert if check name same  แก้ให้ alert แสดงหลัง ทำงานแล้ว

--- webapp/materialDao/group/GroupDao.cs
+++ webapp/materialDao/group/GroupDao.cs
@@ -22,35 +22,35 @@ namespace KKN_UI.material.group
             return conn;
         }
 
-        private const string READ       = "group_itemRead";
-        private const string CREATE     = "group_itemCreate";
-        private const string DELETE     = "group_itemDelete";
-        private const string UPDATE     = "group_itemUpdate";
-                                           
-        private const string READ_BYID  = "group_itemRead_Byid";
+        private const string READ = "group_itemRead";
+        private const string CREATE = "group_itemCreate";
+        private const string DELETE = "group_itemDelete";
+        private const string UPDATE = "group_itemUpdate";
+
+        private const string READ_BYID = "group_itemRead_Byid";
         private const string CHECKGROUPNEW = "CheckGroupNew";
 
         public GroupSQLlist Getdata()
         {
             using (var conn = OpenDbConnection())
             {
-                using (SqlCommand cmd = new SqlCommand(READ,conn))
-                {    
+                using (SqlCommand cmd = new SqlCommand(READ, conn))
+                {
                     cmd.CommandType = CommandType.StoredProcedure;
                     GroupSQLlist result = new GroupSQLlist();
                     //var result = new List<GroupSQL>()
                     using (var rdr = cmd.ExecuteReader())
                     {
-                        while(rdr.Read())
+                        while (rdr.Read())
                         {
                             result.Grouplist.Add(maplistgroupDao(rdr));
                         }
                     }
                     return result;
                 }
-                
+
             }
-                
+
         }
 
         public GroupSQL GetdataByid(int id)
@@ -59,7 +59,7 @@ namespace KKN_UI.material.group
             {
                 using (SqlCommand cmd = new SqlCommand(READ_BYID, conn))
                 {
-                   
+
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.AddWithValue("@group_id", id);
                     GroupSQL result = null;
@@ -81,6 +81,7 @@ namespace KKN_UI.material.group
                 using (SqlCommand cmd = new SqlCommand(CHECKGROUPNEW, conn))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@group_id", groupobject.group_id);
                     cmd.Parameters.AddWithValue("@group_name", groupobject.group_name);
 
                     GroupSQL result = new GroupSQL();
@@ -106,7 +107,7 @@ namespace KKN_UI.material.group
         {
             using (var conn = OpenDbConnection())
             {
-                using (SqlCommand cmd = new SqlCommand(CREATE,conn))
+                using (SqlCommand cmd = new SqlCommand(CREATE, conn))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     //cmd.Parameters.AddWithValue("@group_id", groupobject.group_id);
@@ -116,13 +117,14 @@ namespace KKN_UI.material.group
                     using (var rdr = cmd.ExecuteReader())
                     {
                         rdr.Read();
-                        if(rdr.HasRows)
+                        if (rdr.HasRows)
                         {
                             var t = Convert.ToInt32(rdr["msg"]);
                             if (Convert.ToInt32(rdr["msg"]) == 1)
                             {
                                 result = maplistgroupDao(rdr);
-                            }                        }
+                            }
+                        }
                     }
                     return result;
                 }
@@ -181,7 +183,7 @@ namespace KKN_UI.material.group
         public GroupSQL maplistgroupDao(SqlDataReader rdr)
         {
             var resultgroup = new GroupSQL();
-            resultgroup.group_id = Convert.ToInt32(rdr["group_id"]);  
+            resultgroup.group_id = Convert.ToInt32(rdr["group_id"]);
             resultgroup.group_name = rdr["group_name"].ToString();
 
             return resultgroup;
